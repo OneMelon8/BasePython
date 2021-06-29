864c9234557e3200cd7fa530a82800815f71aa7a
One <48169582+OneMelon8@users.noreply.github.com>
2021-06-29 11:56:42 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,18 @@
+import os
+import sys
+
 import discord
+
+# Stabilize imports
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(current_dir, ".."))  # two directories above
 
 from src.bot import BotClient
 from src.commands import utility_cmd, nlp_cmd, genshin_cmd
 from src.utils.chat_handler import ChatHandler
 from src.data.environment import DISCORD_TOKEN
 
+print("Hello (happy) world!")
 # Create intent
 intent = discord.Intents.default()
 intent.members = True
